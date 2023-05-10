8074bd5aea06df8be1711309f4d8e9c1c41f3145
NetilogyIvan <inkornelyuk@gmail.com>
2023-05-10 21:57:46 +0600

    fix cod SavingAccont

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SavingAccountTest {
-
-    @Test //не складывает начальный баланс и сумму, которую хотим добвить
+//тесты для add (пополнения) баланса:
+    //1) add рандомное число с рандомным балансом
+    @Test //не складывает начальный баланс и сумму, которую хотим добвить/ по фиксино
     public void shouldAddLessThanMaxBalance() {
         SavingAccount account = new SavingAccount(
                 2_000,
@@ -13,12 +14,12 @@ public class SavingAccountTest {
                 10_000,
                 5
         );
-
         account.add(3_000);
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
-    @Test  // добавляем сумму превышающую максимальное значение суммарно. По идее отмена операции и ожидаем 2000. ТЕСТ ПРОШЕЛ, но он не показательный
+    @Test  // добавляем сумму превышающую максимальный баланс суммарно. По идее отмена операции и ожидаем 2000.
+    //2) add рандомное число с рандомным балансом, сумма выше мак баланса - должна быть отмена операции
     public void shouldAddMoreThanMaxBalance() {
         SavingAccount account = new SavingAccount(
                 2_000,
@@ -26,12 +27,11 @@ public class SavingAccountTest {
                 10_000,
                 5
         );
-
         account.add(10_000);
 
         Assertions.assertEquals(2_000, account.getBalance());
     }
-    @Test  // добавляем отрицательное число. хз что должнополучиться. проверка на баг. ХЗ КАК, но тест ПРОШЕЛ
+    @Test  // добавляем отрицательное число./ по фиксино
     public void shouldAddNegativeNumberThanMaxBalance() {
         SavingAccount account = new SavingAccount(
                 2_000,
@@ -39,12 +39,11 @@ public class SavingAccountTest {
                 10_000,
                 5
         );
-
         account.add(-10_000);
 
         Assertions.assertEquals(2_000, account.getBalance());
     }
-    @Test  // списываем отрицательное число. хз что должнополучиться. проверка на баг. ХЗ КАК, но тест ПРОШЕЛ
+    @Test  // списываем отрицательное число. тест прошел
     public void shouldPayNegativeNumberThanMaxBalance() {
         SavingAccount account = new SavingAccount(
                 2_000,
@@ -70,7 +69,7 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(1_000, account.getBalance());
     }
-    @Test  // списываем сумму меньшую, чем наш баланси чтобы остаток был меньше минимального. Вычел. Получил 500. Но должен был выдать ошибку.
+    @Test  // списываем сумму меньшую, чем наш баланси чтобы остаток был меньше минимального./ по фиксино
     public void shouldPayLessThanInitialBalanceAndLessThenMinBalance() {
         SavingAccount account = new SavingAccount(
                 2_000,
@@ -83,7 +82,7 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000, account.getBalance());
     }
-    @Test  // списываем сумму большую, чем наш баланс. Сумму вычел, актуал получился -1000
+    @Test  // списываем сумму большую, чем наш баланс. Сумму вычел, актуал получился -1000 / по фиксино
     public void shouldPayMoreThanInitialBalance() {
         SavingAccount account = new SavingAccount(
                 2_000,
@@ -91,7 +90,6 @@ public class SavingAccountTest {
                 10_000,
                 5
         );
-
         account.pay(3_000);
 
         Assertions.assertEquals(2_000, account.getBalance());
@@ -99,14 +97,13 @@ public class SavingAccountTest {
     @Test  // списываем сумму большую, чем наш баланс. Сумму вычел, актуал получился -1000
     public void testYearChange() {
         SavingAccount account = new SavingAccount(
-                2_000,
+                200,
                 1_000,
                 10_000,
-                10
+                15
         );
-
         int yearChange = account.yearChange();
 
-        Assertions.assertEquals(200, yearChange);
+        Assertions.assertEquals(30, yearChange);
     }
 }
