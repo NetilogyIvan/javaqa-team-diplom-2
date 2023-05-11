b2ac45bc4d843d351a14495ba2d9fa28f04f42ce
NetilogyIvan <inkornelyuk@gmail.com>
2023-05-11 22:56:37 +0600

    Please enter the commit message for your changes. Lines starting  Your branch is up to date with 'origin/credit'.  Changes to be committed:

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class CreditAccountTest {
 
-    @Test //баг, нет сложения начального баланса с добавленным
+    @Test //++ сложения начального баланса с добавленным/ random значения
     public void testShouldAddToPositiveBalance() {
         CreditAccount account = new CreditAccount(
                 1_000,
@@ -17,24 +17,50 @@ public class CreditAccountTest {
         Assertions.assertEquals(4_000, account.getBalance());
     }
 
-    @Test //пройдено
+
+    @Test //++ граничные значения/ сложение начльного баланса + 0
     public void testAddNullToPositiveBalance() {
         CreditAccount account = new CreditAccount(
-                5_000,
+                1,
                 5_000,
                 15
         );
 
         account.add(0);
 
-        Assertions.assertEquals(5_000, account.getBalance());
-        // тут все верно
+        Assertions.assertEquals(1, account.getBalance());
+
     }
 
+    @Test //++ граничные значения/ сложение начального баланса + 1
+    public void testAddOneToPositiveBalance() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                15,
+                1
+        );
 
-    @Test
-//баг, по условию кредитный счет может иметь баланс вплоть до отрицательного, но до указанного кредитного лимита.
-    //кредитный лимит 5000, операция должна завершится и ничего не поменяв на счёте
+        account.add(1);
+
+        Assertions.assertEquals(5_001, account.getBalance());
+
+    }
+
+    @Test //++ граничные значения/ сложение начального баланса 0 + 0
+    public void testAddNullToBalanceNull() {
+        CreditAccount account = new CreditAccount(
+                0,
+                500,
+                423
+        );
+
+        account.add(0);
+
+        Assertions.assertEquals(0, account.getBalance());
+
+    }
+
+    @Test//++ покупка выше кредитного лимита
     public void testPayAboveTheLimit() {
         CreditAccount account = new CreditAccount(0, 5_000, 15);
         account.pay(6_000);
@@ -42,7 +68,7 @@ public class CreditAccountTest {
 
     }
 
-    @Test//успешно, покупка на баланс+кредитный лимит
+    @Test//++ покупка на баланс+кредитный лимит
     public void testPayForTheBalanceAndCreditLimit() {
         CreditAccount account = new CreditAccount(1_000, 5_000, 15);
         account.pay(6_000);
@@ -50,35 +76,42 @@ public class CreditAccountTest {
 
     }
 
-    @Test//баг, покупка на при 0 балансе и 0 кредитный лимит
-    public void testPayForTheBalanceAndCredit() {
-        CreditAccount account = new CreditAccount(0, 0, 15);
-        account.pay(6_000);
+    @Test//++ /граничные значения покупка на баланс+кредитный лимит
+    public void testPayForTheBalanceNullAndCreditLimitNull() {
+        CreditAccount account = new CreditAccount(0, 0, 1);
+        account.pay(1);
         Assertions.assertEquals(0, account.getBalance());
     }
 
-    @Test//успешно, покупка по балансу с 0 лимитом
+    @Test//++ покупка по балансу с 0 лимитом
     public void testPayForTheBalanceForNullCredit() {
-        CreditAccount account = new CreditAccount(6_000, 0, 15);
-        account.pay(6_000);
-        Assertions.assertEquals(0, account.getBalance());
+        CreditAccount account = new CreditAccount(1_000, 0, 15);
+        account.pay(300);
+        Assertions.assertEquals(700, account.getBalance());
     }
 
-    @Test//успешно, считает проценты при отрицательном балансе
+    @Test//++ покупка на кредитный лимит при балансе 0
+    public void testPayForTheBalanceNullAndCreditLimit() {
+        CreditAccount account = new CreditAccount(0, 5_001, 115);
+        account.pay(5_000);
+        Assertions.assertEquals(-5_000, account.getBalance());
+    }
+
+    @Test// ++ считает проценты при отрицательном балансе
     public void testYearChangeNegativeBalance() {
         CreditAccount account = new CreditAccount(-200, 5_000, 15);
         account.yearChange();
         Assertions.assertEquals(-30, account.yearChange());
     }
 
-    @Test//баг, не должен считат проценты при положительном балансе
+    @Test//++ не должен считат проценты при положительном балансе
     public void testYearChangePositiveBalance() {
         CreditAccount account = new CreditAccount(200, 5_000, 15);
         account.yearChange();
-        Assertions.assertEquals(200, account.yearChange());
+        Assertions.assertEquals(0, account.yearChange());
     }
 
-    @Test//баг, не должен считат проценты при положительном балансе
+    @Test//++/граничные значения не должен считат проценты при положительном балансе
     public void testYearChangeBalanceNull() {
         CreditAccount account = new CreditAccount(0, 5_000, 15);
         account.yearChange();
@@ -86,15 +119,24 @@ public class CreditAccountTest {
 
     }
 
-    @Test//успешно, При 0 и отрицательном rate, должен возвращать IllegalArgumentException(
-    // "Накопительная ставка не может быть отрицательной, а у вас: " + rate);
+    @Test ////++Проверка IllegalArgumentException
     public void testYearChangeRateNull() {
-        CreditAccount account = new CreditAccount(200, 5_000, 0);
-        account.yearChange();
-        Assertions.assertEquals(200, account.yearChange());
+        try {
+            CreditAccount account = new CreditAccount(200, 5_000, 0);
+            Assertions.fail("IllegalArgumentException not thrown");
+        } catch (IllegalArgumentException expected) {
 
-
+        }
     }
 
+    @Test ////++Проверка IllegalArgumentException
+    public void testYearChangeRateNegative() {
+        try {
+            CreditAccount account = new CreditAccount(0, 0, -1);
+            Assertions.fail("IllegalArgumentException not thrown");
+        } catch (IllegalArgumentException expected) {
+
+        }
+    }
 
 }
